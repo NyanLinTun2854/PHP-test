54d48a6f57cd783d2c7ef8e89843bd3a4a5bc217
nyanlintun2854 <nyanlintun28504@gmail.com>
2023-06-07 13:22:14 +0630

    first day

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,19 +18,47 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('greeting', fn() => 'Min Ga Lar Par');
+// Route::get('greeting', fn() => 'Min Ga Lar Par');
 
-Route::get('myName', function () {
-    $firstName = "Nyan Lin";
-    $lastName = "Tun";
-    return $firstName." ".$lastName;
+// Route::get('myName', function () {
+//     $firstName = "Nyan Lin";
+//     $lastName = "Tun";
+//     return $firstName." ".$lastName;
+// });
+
+// Route::get('area/{width}/{height?}', function ($width, $height= null) {
+//     return "Area is ". $width ." sqft.";
+// });
+
+// Route::get('products', function () {
+//     $data = file_get_contents('https://fakestoreapi.com/products');
+//     dd(json_decode($data));
+// });
+
+Route::get('greeting', fn() => "Min Ga Lar Par");
+
+Route::get('myName/{firstName}/{secondName?}', function($firstName, $secondName = null) {
+    return "My Name is ".$firstName." ".$secondName.".";
 });
 
-Route::get('area/{width}/{height?}', function ($width, $height= null) {
-    return "Area is ". $width ." sqft.";
-});
-
-Route::get('products', function () {
+Route::get('product', function () {
     $data = file_get_contents('https://fakestoreapi.com/products');
-    dd(json_decode($data));
+    // dd(json_decode($data));
+
+    return $data;
+});
+
+Route::get('products/product-max/{amount}', function ($amount) {
+    // $products = file_get_contents('https://fakestoreapi.com/products');
+    // $productsArr = json_decode($products);
+    // $dataArr = array_filter($productsArr, fn($product) => $product->price < $amount);
+    // dd($dataArr);
+
+    $res = Http::get('https://fakestoreapi.com/products');
+    dd($res->collect()->where('price','<',$amount));
+});
+
+Route::get('products/priceBetween/{min}/and/{max}', function ($min, $max) {
+    $res = Http::get('https://fakestoreapi.com/products');
+    dd($res->collect()->whereBetween('price', [$min, $max]));
 });
